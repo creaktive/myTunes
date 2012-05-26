51f34aa5d444227f68282544a3706ff202e329e8
Mobile User <mobile@Stas-iPhone.(none)>
2012-05-26 17:56:52 -0300

    correct query for iOS5 in myTunes.sh

--- myTunes.sh
+++ myTunes.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 MEDIA="/private/var/mobile/Media"
-LIBFILE="$MEDIA/iTunes_Control/iTunes/iTunes Library.itlp/Library.itdb"
-LOCFILE="$MEDIA/iTunes_Control/iTunes/iTunes Library.itlp/Locations.itdb"
-ITFILES="$MEDIA/iTunes_Control/Music"
+LIBFILE="$MEDIA/iTunes_Control/iTunes/MediaLibrary.sqlitedb"
 MYTUNES="$MEDIA/myTunes"
 
 rm -Rf "$MYTUNES"
@@ -11,13 +9,23 @@ cd "$MYTUNES"
 
 (
 sqlite3 -list "$LIBFILE" << SQLITE_END | tr ':?*' '___'
-	ATTACH '$LOCFILE' AS loc;
-	SELECT '$ITFILES/' || location, title || SUBSTR(location, 9) FROM item LEFT JOIN loc.location ON (pid = item_pid) ORDER BY location ASC;
-	DETACH DATABASE loc;
+    SELECT
+        '$MEDIA/' || path || '/' || location,
+        album_artist || ' - ' || album || ' - ' || title || SUBSTR(location, 5)
+    FROM item
+    LEFT JOIN base_location
+        ON item.base_location_id = base_location.base_location_id
+    LEFT JOIN item_extra
+        ON item.item_pid = item_extra.item_pid
+    LEFT JOIN album_artist
+        ON item.album_artist_pid = album_artist.album_artist_pid
+    LEFT JOIN album
+        ON item.album_pid = album.album_pid;
 SQLITE_END
 ) | while read line
 do
 	file=$(echo $line | cut -d '|' -f1)
 	name=$(echo $line | cut -d '|' -f2)
+	#echo "$file" "$name"
 	$(ln "$file" "$name")
 done
